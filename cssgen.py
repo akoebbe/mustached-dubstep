309227e763a8218ac9b6b38be72444a7609a1436
vml-akoebbe <akoebbe@vml.com>
2014-09-22 13:57:03 -0500

    Fixes that I hope Karl will never look at. If he does, I'll probably loose my job.

--- cssgen.py
+++ cssgen.py
@@ -1,4 +1,6 @@
-itteration = 0
-while itteration < 4000:
-  print ".class-" + str(itteration) + "{ color: blue; font-weight: bold; background: yellow; font-family: sans-serif; border-size: 2px; border-color: blue; margin-left: 1rem; margin-right: 1rem; margin-top; 1rem; margin-bottom: 1rem}"
-  itteration += 1
+# The spelling mistakes are totally Karl's fault because he is a dufus who likes to 
+# wear a skin tight suit and ride on chained linked two-wheeled contraption
+iteration = 0
+while iteration < 4000:
+  print ".class-" + str(iteration) + "{ color: blue; font-weight: bold; background: yellow; font-family: sans-serif; border-size: 2px; border-color: blue; margin-left: 1rem; margin-right: 1rem; margin-top; 1rem; margin-bottom: 1rem}"
+  iteration += 1
